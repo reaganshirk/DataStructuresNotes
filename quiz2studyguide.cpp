aa9ff1514bdb64bc371f3b26431012a467fe3818
Reagan Shirk <reaganshirk@ou.edu>
2019-03-28 12:39:46 -0500

    what we've gone over since the exam broken down by what Sridhar mentioned might be on the quiz versus what he hasn't said will be

---
+++ quiz2studyguide.cpp
@@ -0,0 +1,35 @@
+/*
+
+- Things Sridhar mentioned would be on the quiz or exam:
+	- From March 11th
+		- Queues
+			- How to tell if a queue is full
+			- Probably want to know class definition for array and LinkedList implementations
+		- Radix Sort ** mentioned it's one of his favorite questions **
+			- Queue implentation
+			- Know the algorithm
+	- From March 13th
+		- Maximum number of non-empty nodes in a binary tree of height h
+		- Definition of a full binary tree
+		- Minimum height of a binary tree with n number of nodes
+	- From March 25th
+		- Recursive algorithms for pre order, in order, post order traversals
+		- Non-recursive algorithms for pre order, in order traversals
+	- From March 27th
+		- Number of ways to store a binary tree
+
+- Things we went over but were not said would be on quiz or exam:
+	- From March 11th
+		- Simple bucket sort
+			- Complexity 
+			- Problems
+		- Radix sort
+			- Complexity
+			- The one problem
+	- From March 13th
+		- Operations
+	- From March 25th
+		- Project 4 information
+	- From March 27th
+		- Binary Search Tree
+*/
