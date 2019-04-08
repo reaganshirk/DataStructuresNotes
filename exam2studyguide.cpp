d8ade5817e5af67d958bc7df0a46df1292415207
Reagan Shirk <reaganshirk@ou.edu>
2019-04-08 10:37:03 -0500

    study guide for the second midterm

---
+++ exam2studyguide.cpp
@@ -0,0 +1,51 @@
+/*
+- Things Sridhar mentioned would be on the exam:
+	- From March 11th
+		- Queues
+			- How to tell if a queue is full
+			- Probably want to know class definition for array and LinkedList implementations
+		- Radix Sort ** mentioned it's one of his favorite questions **
+			- Queue implentation
+			- Know the algorithm
+			- Know how to do by hand
+	- From March 13th
+		- Maximum number of non-empty nodes in a binary tree of height h
+		- Definition of a full binary tree
+		- Minimum height of a binary tree with n number of nodes
+	- From March 25th
+		- Recursive algorithms for pre order, in order, post order traversals
+		- Non-recursive algorithms for pre order, in order traversals
+	- From March 27th
+		- Number of ways to store a binary tree
+	- From April 1st
+		- Give template for a zig/zag
+		- Give sequence of C++ assignments to give a zag operation
+		- Given a BST, do a splay. Give all rotations and such
+		- Given a splay tree, do a find on a given number (rotate until that number is the node)
+	- From April 3rd
+		- Splay trees
+			- Know how to do top down and bottom up
+		- AVL trees
+			- Inserting numbers and fixing violations
+
+- Things we went over but were not said would be on exam:
+	- From March 11th
+		- Simple bucket sort
+			- Complexity
+			- Problems
+		- Radix sort
+			- Complexity
+			- The one problem
+	- From March 13th
+		- Operations
+	- From March 25th
+		- Project 4 information
+	- From March 27th
+		- Binary Search Tree
+	- From April 1st
+		- Finding values in BST
+		- Removing values in BST
+		- BST complexity
+	- From April 3rd
+		- Looks like it's all likely to be on the exam
+*/
