afc55e46ff419f2c701cd41076f930673a11b4ad
Reagan Shirk <reaganshirk@ou.edu>
2019-04-24 10:32:02 -0500

    added april 17

--- quiz3studyguide.cpp
+++ quiz3studyguide.cpp
@@ -15,4 +15,38 @@ April 15:
 	- Forrest of Trees: When each connected component is a tree
 	- Given an n-node directed graph, the maximum number of edges is n^2 - n. For undirected: n^2 - n / 2. Size: O(n^2)
 	- Adjacency list: size is O(n + m)
+April 17:
+	- Triangular matricies: Draw a line through the diagonal of a matrix. Can store either above the line or below the line. Do not store
+	  the boxes that the line goes through. Can be stored in array or unsigned characters and bits.
+	- Depth First Search: Pick a node, go to its neighbors, mark it visited, repeat. Once you get to a node with all visited neighbors,
+	  work backwards. End up with a tree that will be stored in a parent array. Spanning tree.
+	dfs (int x)
+	{
+		visited[x] = true;
+		for (each neighbor y of x)
+		{
+			if (!visited[y])
+			{
+				parent[y] = x;
+				dfs(y);
+			}
+		}
+	}
+	- Breadth First Search: Pick a node, make its neighbors its children. Go to one child and do the same, go to other child and do the same.
+	  Skip any neighbor that has already been visited.
+	 int bfs (int x)
+	 {
+		Q. enqueue(x);
+		visited[x] = true;
+		while (!Q.isEmpty())
+		{
+			k = Q.dequeue();
+			for (each unvisited neighbor y of k)
+			{
+				parent[y] = k;
+				visited[y] = true;
+				Q.enqueue(y);
+			}
+		}
+	}
 */
